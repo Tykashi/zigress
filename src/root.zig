a57ecce029fecf91bb4d076a408133bca9c6fc35
Tykashi <me@tykashi.dev>
2025-05-17 06:28:43 +0000

    fix: added ServerConfig pub export

--- src/root.zig
+++ src/root.zig
@@ -1,3 +1,4 @@
 pub const Server = @import("lib/server.zig").Server;
 pub const Request = @import("lib/request.zig").Request;
 pub const Response = @import("lib/response.zig").Response;
+pub const ServerConfig = @import("lib/server_config.zig").ServerConfig;
